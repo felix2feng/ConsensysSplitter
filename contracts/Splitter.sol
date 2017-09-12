fc0796276d300173cf746da9d55e06fd83dc234f
Felix <felix2feng@gmail.com>
2017-09-11 23:23:39 -0400

    Build out splitter

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -1,27 +1,102 @@
 pragma solidity ^0.4.4;
 
-contract MetaCoin {
-	mapping (address => uint) balances;
+contract Owned {
+    address owner;
+    
+    function Owned () {
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner () {
+        require(msg.sender == owner);
+        _;
+    }
+}
 
-	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+contract Splitter is Owned {
+    struct Person {
+        uint balance;
+        address[] splittees;
+    }
+    
+	mapping (address => Person) people;
 
-	function MetaCoin() {
-		balances[tx.origin] = 10000;
+	function Splitter() {}
+	
+	function addSplittee(address splittee) 
+	    returns (bool)
+	{
+	    people[msg.sender].splittees.push(splittee);
+	    return true;
 	}
 
-	function sendCoin(address receiver, uint amount) returns(bool sufficient) {
-		if (balances[msg.sender] < amount) return false;
-		balances[msg.sender] -= amount;
-		balances[receiver] += amount;
-		Transfer(msg.sender, receiver, amount);
-		return true;
+	function getContractBalance() 
+	    constant 
+	    public
+	    returns(uint) 
+    {
+		return this.balance;
+	}
+    
+	function getPersonBalance(address _address) 
+	    public
+	    constant
+	    returns(uint) 
+    {
+	    return people[_address].balance;
+	}
+	
+	function withdrawBalance() 
+	    public
+	    returns(bool)
+	{
+	    uint balanceToWithdraw = people[msg.sender].balance;
+	    
+	    // Require that the requester has a balance
+	    require(balanceToWithdraw > 0);
+	    
+	    // Set to 0 ahead of time to prevent reentrance attacks
+	    people[msg.sender].balance = 0;
+	    
+	    if (!msg.sender.send(balanceToWithdraw)) {
+            // Reset balance if the send has failed
+            people[msg.sender].balance = balanceToWithdraw;
+            return false;
+        }
+	    return true;
 	}
 
-	function getBalanceInEth(address addr) returns(uint){
-		return ConvertLib.convert(getBalance(addr),2);
+	function split () 
+	    payable
+	    public
+	    returns (bool success) 
+    {
+        // Check that there is a balance
+        require(msg.value > 0);
+        
+        // Check that the sender has splittees
+        address[] memory splittees = people[msg.sender].splittees;
+        uint numSplittees = splittees.length;
+        require(numSplittees > 0);
+        
+        // Split amount
+        uint splitAmount = msg.value / numSplittees;
+        
+        // Increment balances of each splittee
+        for (uint i = 0; i < numSplittees; i++) {
+            address splitteeAddress = splittees[i];
+            people[splitteeAddress].balance += splitAmount;
+        }
+        
+        return true;
 	}
-
-	function getBalance(address addr) returns(uint) {
-		return balances[addr];
+	
+	function killContract () 
+	    onlyOwner()
+	    returns (bool)
+	{
+	    // All ether in the contract is sent to the owner
+	    // and the storage and code is removed from state
+	    selfdestruct(owner);
 	}
 }
